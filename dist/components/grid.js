c46472133493a097ed48eb6ab81fb8e66fd22789
Pavel <boyko.pavel@phobosys.com>
2022-03-31 18:24:48 +0200

    change eslint rule to warn on no-console; implement horizontal lines for Grid; grid to graphics inherit;

--- dist/components/grid.js
+++ dist/components/grid.js
@@ -2,28 +2,66 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Grid = void 0;
 const graphics_1 = require("@pixi/graphics");
-class Grid {
-    constructor(stickChart) {
-        this.stickChart = stickChart;
+class Grid extends graphics_1.Graphics {
+    constructor({ width, height, dateRange, segmentDateInterval, stickDateInterval, valueInterval, valueRange }) {
+        super();
+        this.gridWidth = width;
+        this.gridHeight = height;
+        this.dateRange = dateRange;
+        this.segmentDateInterval = segmentDateInterval;
+        this.stickDateInterval = stickDateInterval;
+        this.valueRange = valueRange;
+        this.valueInterval = valueInterval;
     }
-    draw(container) {
+    build() {
+        this.buildVerticalLines();
+        this.buildHorizontalLines();
+        return this;
+    }
+    buildVerticalLines() {
         const verticalSegmentsCount = this.verticalSegmentsCount();
         for (let i = 0; i < verticalSegmentsCount; i++) {
             const pos = i * this.widthForSegment();
             const line = new graphics_1.Graphics();
-            line.lineStyle({ width: 1, color: 0xFFFF }).moveTo(pos, 0).lineTo(pos, this.stickChart.height).endFill();
-            container.addChild(line);
+            line
+                .lineStyle({ width: 1, color: 0xFFFF })
+                .moveTo(pos, 0)
+                .lineTo(pos, this.gridHeight);
+            super.addChild(line);
+        }
+    }
+    buildHorizontalLines() {
+        const horizontalSegmentsCount = this.horizontalSegmentsCount();
+        for (let i = 0; i < horizontalSegmentsCount; i++) {
+            const pos = i * this.heightForSegment();
+            const line = new graphics_1.Graphics();
+            line
+                .lineStyle({ width: 1, color: 0xFFFF })
+                .moveTo(0, pos)
+                .lineTo(this.gridWidth, pos)
+                .endFill();
+            super.addChild(line);
         }
     }
     widthForSegment() {
-        return this.stickChart.width / this.verticalSegmentsCount();
+        return this.gridWidth / this.verticalSegmentsCount();
+    }
+    heightForSegment() {
+        return this.gridHeight / this.horizontalSegmentsCount();
     }
     verticalSegmentsCount() {
-        const verticalSegmentsCount = this.stickChart.dateRange.milliseconds / this.stickChart.segmentDateInterval.milliseconds;
+        const verticalSegmentsCount = this.dateRange.milliseconds / this.segmentDateInterval.milliseconds;
         if (verticalSegmentsCount < 1) {
             throw new Error('DateRange could\'t be smaller than the Interval. It should contain 1 or more intervals');
         }
         return verticalSegmentsCount;
+    }
+    horizontalSegmentsCount() {
+        const horizontalSegmentsCount = this.valueRange.value / this.valueInterval.value;
+        if (horizontalSegmentsCount < 1) {
+            throw new Error('DateRange could\'t be smaller than the Interval. It should contain 1 or more intervals');
+        }
+        return horizontalSegmentsCount;
     }
 }
 exports.Grid = Grid;
--- src/components/grid.ts
+++ src/components/grid.ts
@@ -1,15 +1,45 @@
-import { Container } from '@pixi/display'
 import { Graphics } from '@pixi/graphics'
 
 import { IStickChart } from '../types/stick_chart'
-export class Grid {
-    protected stickChart: IStickChart
+import { DateRange } from '../utils/date_range'
+import { ValueRange } from '../utils/value_range'
+export class Grid extends Graphics {
+    gridWidth: number
 
-    constructor(stickChart) {
-        this.stickChart = stickChart
+    gridHeight: number
+
+    dateRange: DateRange
+
+    segmentDateInterval: DateRange
+
+    stickDateInterval: DateRange
+
+    valueRange: ValueRange
+
+    valueInterval: ValueRange
+
+    constructor({ width, height, dateRange, segmentDateInterval, stickDateInterval, valueInterval, valueRange }: IStickChart) {
+        super()
+
+        this.gridWidth = width
+        this.gridHeight = height
+
+        this.dateRange = dateRange
+        this.segmentDateInterval = segmentDateInterval
+        this.stickDateInterval = stickDateInterval
+
+        this.valueRange = valueRange
+        this.valueInterval = valueInterval
     }
 
-    draw(container: Container): void {
+    build(): Grid {
+        this.buildVerticalLines()
+        this.buildHorizontalLines()
+
+        return this
+    }
+
+    private buildVerticalLines(): void {
         const verticalSegmentsCount = this.verticalSegmentsCount()
 
         for (let i = 0; i < verticalSegmentsCount; i++) {
@@ -17,24 +47,60 @@ export class Grid {
 
             const line = new Graphics()
 
-            line.lineStyle({ width: 1, color: 0xFFFF }).moveTo(pos, 0).lineTo(pos, this.stickChart.height).endFill()
+            line
+                .lineStyle({ width: 1, color: 0xFFFF })
+                .moveTo(pos, 0)
+                .lineTo(pos, this.gridHeight)
 
-            container.addChild(line)
+            super.addChild(line)
+        }
+    }
+
+    private buildHorizontalLines(): void {
+        const horizontalSegmentsCount = this.horizontalSegmentsCount()
+
+        for (let i = 0; i < horizontalSegmentsCount; i++) {
+            const pos = i * this.heightForSegment()
+
+            const line = new Graphics()
+
+            line
+                .lineStyle({ width: 1, color: 0xFFFF })
+                .moveTo(0, pos)
+                .lineTo(this.gridWidth, pos)
+                .endFill()
+
+            super.addChild(line)
         }
     }
 
     private widthForSegment(): number {
-        return this.stickChart.width / this.verticalSegmentsCount()
+        return this.gridWidth / this.verticalSegmentsCount()
+    }
+
+    private heightForSegment(): number {
+        return this.gridHeight / this.horizontalSegmentsCount()
     }
 
     private verticalSegmentsCount(): number {
         const verticalSegmentsCount =
-            this.stickChart.dateRange.milliseconds / this.stickChart.segmentDateInterval.milliseconds
+            this.dateRange.milliseconds / this.segmentDateInterval.milliseconds
 
         if (verticalSegmentsCount < 1) {
             throw new Error('DateRange could\'t be smaller than the Interval. It should contain 1 or more intervals')
         }
 
         return verticalSegmentsCount
+    }
+
+    private horizontalSegmentsCount(): number {
+        const horizontalSegmentsCount =
+            this.valueRange.value / this.valueInterval.value
+
+        if (horizontalSegmentsCount < 1) {
+            throw new Error('DateRange could\'t be smaller than the Interval. It should contain 1 or more intervals')
+        }
+
+        return horizontalSegmentsCount
     }
 }
